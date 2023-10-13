ff817e5abbad92ce805da084ae9894715719c59d
Maher Kefi <kefi.maher@esprit.tn>
2023-10-13 11:11:50 +0100

    13 10 2023  11:11

--- src/Test.java
+++ src/Test.java
@@ -2,41 +2,72 @@ import java.util.ArrayList;
 import java.util.List;
 public class Test {
     public static void main (String [] args) {
-        ArrayList list = new ArrayList() ;
-      //  List<Pesrsonne> P = new ArrayList<>();
+        ArrayList list = new ArrayList();
         list.add(5);
         list.add(6);
         list.add("maher");
         list.add(5);
         list.add("kefi");
-        Integer a=5 ;
+        Integer a = 5;
         System.out.println(list);
         System.out.println(list.contains("test"));
         System.out.println(list.contains(5));
         System.out.println(list.remove(5));
         System.out.println(list.remove((a)));
         System.out.println(list.remove(("test")));
-        Pesrsonne p1 = new Pesrsonne(1,15,"KEFI","MAHER") ;
-        Pesrsonne p2 = new Pesrsonne(2,16,"Riyahi","EYA") ;
-        Pesrsonne p3 = new Pesrsonne(3,17,"KEFI","MAHA") ;
-        Pesrsonne p4 = new Pesrsonne(4,18,"Esser","Fatnassi") ;
-        list.add(p1) ;
-        list.add(p2) ;
-        list.add(p3) ;
-        list.add(p4) ;
+        Pesrsonne p1 = new Pesrsonne(1, 15, "KEFI", "MAHER");
+        Pesrsonne p2 = new Pesrsonne(2, 16, "Riyahi", "EYA");
+        Pesrsonne p3 = new Pesrsonne(3, 17, "KEFI", "MAHA");
+        Pesrsonne p4 = new Pesrsonne(4, 18, "Esser", "Fatnassi");
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        list.add(p4);
         System.out.println(list);
         System.out.println(list.size());
         System.out.println(list.get(8));
         System.out.println(list.get((5)));
+
+        Object v1 = list.get(0);
+
 // parcourie la liste
 
-        for (Pesrsonne personne : personne) {
-            System.out.println(personne);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        for (Object var:list) {
+            System.out.println(var);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+
+            Object ob=list.get(i);
+            if (ob instanceof Pesrsonne)
+            {
+                Pesrsonne p = (Pesrsonne) ob ;
+                System.out.println("nom"+p.getNom());
+            }
+
+
+        }
+
+        for (Object obj:list)
+        {
+            if (obj instanceof Pesrsonne)
+            {
+
+                System.out.println(((Pesrsonne)obj).getNom());
+            }
+            ArrayList <Pesrsonne> list1 = new ArrayList<>();
+            list1.add(p1);
+            list1.add(p2);
+            list1.add(p3);
+            list1.add(p4);
+
         }
 
 
-
-// parcourire et afficher que les nom des pesonne avec deux methode
 
 
 
