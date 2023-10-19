ec1177bc9e50e6e552907e223974510cbcf47f68
Maher Kefi <kefi.maher@esprit.tn>
2023-10-19 19:42:57 +0100

    19:42'  19/10/2023

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Test {
-    public static void main (String [] args) {
-        ArrayList list = new ArrayList();
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList();
         list.add(5);
         list.add(6);
         list.add("maher");
@@ -12,8 +16,8 @@ public class Test {
         System.out.println(list.contains("test"));
         System.out.println(list.contains(5));
         System.out.println(list.remove(2));
-        System.out.println(list.remove((a)));
-        System.out.println(list.remove(("test")));
+        System.out.println(list.remove(a));
+        System.out.println(list.remove("test"));
         Pesrsonne p1 = new Pesrsonne(1, 15, "KEFI", "MAHER");
         Pesrsonne p2 = new Pesrsonne(2, 16, "Riyahi", "EYA");
         Pesrsonne p3 = new Pesrsonne(3, 17, "KEFI", "MAHA");
@@ -22,55 +26,23 @@ public class Test {
         list.add(p2);
         list.add(p3);
         list.add(p4);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.get(8));
-        System.out.println(list.get((5)));
-        Object v1 = list.get(0);
-// parcourie la liste
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
-        for (Object var:list) {
+
+        // Sort the list of Pesrsonne objects by age
+        Collections.sort(list, new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof Pesrsonne && o2 instanceof Pesrsonne) {
+                    Pesrsonne p1 = (Pesrsonne) o1;
+                    Pesrsonne p2 = (Pesrsonne) o2;
+                    return Integer.compare(p1.getAge(), p2.getAge());
+                }
+                return 0;
+            }
+        });
+
+        System.out.println("Sorted list by age:");
+        for (Object var : list) {
             System.out.println(var);
         }
-        for (int i = 0; i < list.size(); i++) {
-            Object ob=list.get(i);
-            if (ob instanceof Pesrsonne)
-            {
-                Pesrsonne p = (Pesrsonne) ob ;
-                System.out.println("nom"+p.getNom());
-            }
-        }
-        for (Object obj:list)
-        {
-            if (obj instanceof Pesrsonne)
-            {
-                System.out.println(((Pesrsonne)obj).getNom());
-            }
-            ArrayList <Pesrsonne> list1 = new ArrayList<>();
-            list1.add(p1);
-            list1.add(p2);
-            list1.add(p3);
-            list1.add(p4);
-            System.out.println(list1.contains(p1));
-            System.out.println(list1.contains(p4));
-            System.out.println(list1.contains(p3));
-            System.out.println(list.contains(new Pesrsonne(2,17,"maher" , "xdlol")));
-            ArrayList<String> lists = new ArrayList<>();
-        }
-
-
-
-
-
-
-
-
     }
-
-
-
-
-
 }
